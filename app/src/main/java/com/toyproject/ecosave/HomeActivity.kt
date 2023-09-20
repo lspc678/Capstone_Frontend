d6a2d545764de0ce2d33588038e830aa4a95f083
lspc678 <dltmdqls1945@naver.com>
2023-09-20 20:57:18 +0900

    toolbar, add device button is added at HomeActivity

--- app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
@@ -1,6 +1,6 @@
 package com.toyproject.ecosave
 
-import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.toyproject.ecosave.databinding.ActivityHomeBinding
@@ -13,6 +13,16 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = title
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // 앱바에 back 버튼 활성화
+
         binding.textMyResident.text = "인천광역시 미추홀구 아암대로 15 인하한양아이클래스 xxxx호"
+
+        binding.btnAddDevice.setOnClickListener {
+            val intent = Intent(this, AddDeviceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
