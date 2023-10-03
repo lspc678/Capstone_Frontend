c17eb26d07a5923ee680e02613a8e92e1d13f564
lspc678 <dltmdqls1945@naver.com>
2023-10-03 20:00:47 +0900

    add bitmap rotate when camera intent is used

--- app/src/main/java/com/toyproject/ecosave/AddDeviceActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/AddDeviceActivity.kt
@@ -12,7 +12,6 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
@@ -27,12 +26,17 @@ import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
 
+
 class AddDeviceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddDeviceBinding
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private var photoFile: File? = null
     val CAPTURE_IMAGE_REQUEST = 1
     private var mCurrentPhotoPath: String? = null
+
+    private fun displayMessage(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
 
     @SuppressLint("QueryPermissionsNeeded")
     private fun captureImage() {
@@ -65,8 +69,23 @@ class AddDeviceActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            var exif: ExifInterface? = null
+
+            try {
+                exif = ExifInterface(photoFile!!.absolutePath)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+
+            val orientation = exif?.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
+
             val bitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath)
-            binding.deviceImage.setImageBitmap(bitmap)
+            val cameraRotated = rotateBitmap(bitmap, orientation)
+            binding.deviceImage.setImageBitmap(cameraRotated)
+            // binding.deviceImage.setImageBitmap(bitmap)
         } else {
             displayMessage(baseContext, "Request cancelled or something went wrong.")
         }
@@ -74,7 +93,7 @@ class AddDeviceActivity : AppCompatActivity() {
 
     @SuppressLint("SimpleDateFormat")
     @Throws(IOException::class)
-    private fun createImageFile() : File {
+    private fun createImageFile() : File? {
         // Create an image file name
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val imageFileName = "JPEG_" + timeStamp + "_"
@@ -90,27 +109,37 @@ class AddDeviceActivity : AppCompatActivity() {
         return image
     }
 
-    private fun displayMessage(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-    }
-
-    @Throws(IOException::class)
-    private fun rotateImageIfRequired(img: Bitmap, selectedImage: Uri): Bitmap? {
-        val ei = ExifInterface(selectedImage.path!!)
-        return when (ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(img, 90.0F)
-            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(img, 180.0F)
-            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(img, 270.0F)
-            else -> img
-        }
-    }
-
-    private fun rotateImage(img: Bitmap, degree: Float): Bitmap? {
+    private fun rotateBitmap(bitmap: Bitmap, orientation: Int?): Bitmap? {
         val matrix = Matrix()
-        matrix.postRotate(degree)
-        val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.width, img.height, matrix, true)
-        img.recycle()
-        return rotatedImg
+        when (orientation) {
+            ExifInterface.ORIENTATION_NORMAL -> return bitmap
+            ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> matrix.setScale(-1F, 1F)
+            ExifInterface.ORIENTATION_ROTATE_180 -> matrix.setRotate(180F)
+            ExifInterface.ORIENTATION_FLIP_VERTICAL -> {
+                matrix.setRotate(180F)
+                matrix.postScale(-1F, 1F)
+            }
+            ExifInterface.ORIENTATION_TRANSPOSE -> {
+                matrix.setRotate(90F)
+                matrix.postScale(-1F, 1F)
+            }
+            ExifInterface.ORIENTATION_ROTATE_90 -> matrix.setRotate(90F)
+            ExifInterface.ORIENTATION_TRANSVERSE -> {
+                matrix.setRotate(-90F)
+                matrix.postScale(-1F, 1F)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> matrix.setRotate(-90F)
+            else -> return bitmap
+        }
+
+        return try {
+            val bmRotated: Bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+            bitmap.recycle()
+            bmRotated
+        } catch (ex: OutOfMemoryError) {
+            ex.printStackTrace()
+            null
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
