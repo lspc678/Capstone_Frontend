96d2eae829daf2148c83ffcba34f5fc22f8b2861
lspc678 <dltmdqls1945@naver.com>
2023-10-15 20:15:30 +0900

    login for guest button is added to LoginActivity

--- app/src/main/java/com/toyproject/ecosave/LoginActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.toyproject.ecosave.databinding.ActivityLoginBinding
+import com.toyproject.ecosave.models.LoginRequestBody
+import com.toyproject.ecosave.services.LoginService
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -13,7 +15,21 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val userData = LoginRequestBody(
+            binding.textInputEmail.editText?.text.toString(),
+            binding.textInputPassword.editText?.text.toString()
+        )
+
         binding.btnLogin.setOnClickListener {
+            val loginService = LoginService(userData)
+            loginService.work()
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // 테스트용 버튼이며 정식 버전에서는 삭제 예정
+        binding.btnLoginForGuest.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
