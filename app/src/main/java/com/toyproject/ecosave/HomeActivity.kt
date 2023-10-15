c11f4765a773899fde5c47f341b38f40d0e07168
lspc678 <dltmdqls1945@naver.com>
2023-10-15 21:28:04 +0900

    add drawer to HomeActivity

--- app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
@@ -3,10 +3,13 @@ package com.toyproject.ecosave
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.toyproject.ecosave.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,11 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = title
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 앱바에 back 버튼 활성화
+
+        drawerLayout = binding.drawerLayout
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         binding.textMyResident.text = "인천광역시 미추홀구 아암대로 15 인하한양아이클래스 xxxx호"
 
