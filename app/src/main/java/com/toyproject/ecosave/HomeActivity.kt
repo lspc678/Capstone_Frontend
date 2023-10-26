35832b51e37d05955e7c887eb48a11a1b2494576
lspc678 <dltmdqls1945@naver.com>
2023-10-26 21:42:00 +0900

    add recyclerView at HomeActivity

--- app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
@@ -1,15 +1,35 @@
 package com.toyproject.ecosave
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 import com.toyproject.ecosave.databinding.ActivityHomeBinding
+
+import com.toyproject.ecosave.models.RelativeElectricPowerConsumeGradeData
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var drawerLayout: DrawerLayout
+
+    private var recyclerView: RecyclerView? = null
+    private var recyclerViewRegisteredDeviceListAdapter: RecyclerViewRegisteredDeviceListAdapter? = null
+    private var list = mutableListOf<RelativeElectricPowerConsumeGradeData>()
+
+    private fun prepareListData() {
+        var data = RelativeElectricPowerConsumeGradeData(0, 1, 10, 35.9F, 0)
+        list.add(data)
+
+        data = RelativeElectricPowerConsumeGradeData(1, 2, 15, 131.3F, 0)
+        list.add(data)
+
+        data = RelativeElectricPowerConsumeGradeData(5, 3, 28, 83.0F, 1)
+        list.add(data)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +40,15 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.title = title
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 앱바에 back 버튼 활성화
+
+        list = ArrayList()
+        recyclerView = binding.recyclerView
+
+        recyclerViewRegisteredDeviceListAdapter = RecyclerViewRegisteredDeviceListAdapter(this, list)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        recyclerView!!.layoutManager = layoutManager
+        recyclerView!!.adapter = recyclerViewRegisteredDeviceListAdapter
+        prepareListData()
 
         drawerLayout = binding.drawerLayout
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
