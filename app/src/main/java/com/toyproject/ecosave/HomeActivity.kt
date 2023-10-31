8ea3e98b76fda8fd17e97d79de3747d2a965a8af
lspc678 <dltmdqls1945@naver.com>
2023-10-31 19:34:12 +0900

    implement change my residence function

--- app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/HomeActivity.kt
@@ -1,16 +1,30 @@
 package com.toyproject.ecosave
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.Manifest
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+import com.toyproject.ecosave.apis.naverapi.ReverseGeocodingAPI
 import com.toyproject.ecosave.databinding.ActivityHomeBinding
-
 import com.toyproject.ecosave.models.RelativeElectricPowerConsumeGradeData
+import com.toyproject.ecosave.models.ReverseGeocodingResponse
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -19,6 +33,18 @@ class HomeActivity : AppCompatActivity() {
     private var recyclerView: RecyclerView? = null
     private var recyclerViewRegisteredDeviceListAdapter: RecyclerViewRegisteredDeviceListAdapter? = null
     private var list = mutableListOf<RelativeElectricPowerConsumeGradeData>()
+
+    companion object {
+        const val REQUEST_CODE_PERMISSIONS = 1001
+        private val permissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        private val optionsForChangeMyResidence = arrayOf(
+            "현재 위치를 거주지로 설정",
+            "지도에서 검색"
+        )
+    }
 
     private fun prepareListData() {
         var data = RelativeElectricPowerConsumeGradeData(0, 1, 10, 35.9F, 0)
@@ -29,6 +55,154 @@ class HomeActivity : AppCompatActivity() {
 
         data = RelativeElectricPowerConsumeGradeData(5, 3, 28, 83.0F, 1)
         list.add(data)
+    }
+
+    private fun showWarningDialogForChangeMyResidence() {
+        val alertDialogBuilderBtn = AlertDialog.Builder(this@HomeActivity)
+        alertDialogBuilderBtn.setTitle("내 거주지 변경")
+        alertDialogBuilderBtn.setMessage("현재 위치를 기준으로 거주지를 변경합니다.\n주의: 이전에 저장된 거주지 정보는 사라집니다.")
+        alertDialogBuilderBtn.setPositiveButton("확인") { _, _ ->
+            getMyLocation()
+        }
+        alertDialogBuilderBtn.setNegativeButton("취소") { _, _ -> }
+
+        val alertDialogBox = alertDialogBuilderBtn.create()
+        alertDialogBox.show()
+    }
+
+    private fun getMyLocation() {
+        if (permissions.all { ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED}) {
+            val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            var locationCurrent = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+            // 위치 서비스가 켜져있는지 확인
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                turnOnGPS() // 위치 서비스 켜기
+                return
+            }
+
+            if (locationCurrent == null) {
+                locationCurrent = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            }
+
+            val currentLatitude = locationCurrent?.latitude // 위도
+            val currentLongitude = locationCurrent?.longitude // 경도
+
+            if (currentLatitude != null && currentLongitude != null) {
+                searchAddress(currentLatitude, currentLongitude)
+            }
+        } else {
+            requestPermissions(permissions, REQUEST_CODE_PERMISSIONS)
+        }
+    }
+
+    private fun turnOnGPS() {
+        // 위치 설정이 켜져 있지 않으면 위치 설정창으로 이동
+        val alertDialogBuilderBtn = AlertDialog.Builder(this@HomeActivity)
+        alertDialogBuilderBtn.setTitle("위치 서비스 권한 필요")
+        alertDialogBuilderBtn.setMessage("내 거주지 설정을 하기 위해서는 위치 서비스 권한이 필요합니다.")
+        alertDialogBuilderBtn.setPositiveButton("확인") { _, _ ->
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            intent.addCategory(Intent.CATEGORY_DEFAULT)
+            startActivity(intent)
+        }
+        alertDialogBuilderBtn.setNegativeButton("취소") { _, _ -> }
+
+        val alertDialogBox = alertDialogBuilderBtn.create()
+        alertDialogBox.show()
+    }
+
+    // 현재 위치를 기반으로 지번 주소와 도로명 주소를 찾음
+    private fun searchAddress(latitude: Double, longitude: Double) {
+        val api = ReverseGeocodingAPI.create()
+
+        val coords = "$longitude,$latitude" // 경도, 위도
+        Log.d("위치", coords)
+        api.searchAddressByPoint(coords, "json", "addr,roadaddr").enqueue(
+            object : Callback<ReverseGeocodingResponse> {
+                override fun onResponse(
+                    call: Call<ReverseGeocodingResponse>,
+                    response: Response<ReverseGeocodingResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val result = response.body()
+                        if (result != null) {
+                            val addrArea = result.results[0]
+                            val addrRegion = addrArea.region
+                            val addrLand = addrArea.land
+                            val addr = if (addrLand.number2 == "") {
+                                "${addrRegion.area1.name} " +
+                                        "${addrRegion.area2.name} " +
+                                        "${addrRegion.area3.name} " +
+                                        addrLand.number1
+                            } else {
+                                "${addrRegion.area1.name} " +
+                                        "${addrRegion.area2.name} " +
+                                        "${addrRegion.area3.name} " +
+                                        "${addrLand.number1}-${addrLand.number2}"
+                            }
+
+                            val roadAddrArea = result.results[1]
+                            val roadAddrRegion = roadAddrArea.region
+                            val roadAddrLand = roadAddrArea.land
+                            val roadAddr = if (roadAddrLand.number2 == "") {
+                                "${roadAddrRegion.area1.name} " +
+                                        "${roadAddrRegion.area2.name} " +
+                                        "${roadAddrLand.name} " +
+                                        roadAddrLand.number1
+                            } else {
+                                "${roadAddrRegion.area1.name} " +
+                                        "${roadAddrRegion.area2.name} " +
+                                        "${roadAddrLand.name} " +
+                                        "${roadAddrLand.number1}-${roadAddrLand.number2}"
+                            }
+
+                            chooseAddress(addr, roadAddr)
+                        }
+                    } else {
+                        Log.d("위치", response.errorBody()?.string()!!)
+                    }
+                }
+
+                override fun onFailure(call: Call<ReverseGeocodingResponse>, t: Throwable) {
+                    Toast.makeText(this@HomeActivity, "주소 정보를 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                    Log.d("위치", t.message.toString())
+                }
+            }
+        )
+    }
+
+    // Dialog를 통해 지번 주소와 도로명 주소중 하나를 거주지로 설정
+    private fun chooseAddress(addr: String, roadAddr: String) {
+        val options = arrayOf(
+            addr,
+            roadAddr
+        )
+
+        var selected = 1
+
+        val alertDialogBuilderBtn = AlertDialog.Builder(this@HomeActivity)
+        alertDialogBuilderBtn.setTitle("거주지로 설정할 주소를 선택해 주세요.")
+        alertDialogBuilderBtn.setSingleChoiceItems(options, 1) { _, which ->
+            when (which) {
+                which -> selected = which
+            }
+        }
+        alertDialogBuilderBtn.setPositiveButton("확인") { _, _ ->
+            binding.textMyResident.text = options[selected]
+            when (selected) {
+                0 -> {
+                    Toast.makeText(this@HomeActivity, "지번 주소로 설정", Toast.LENGTH_SHORT).show()
+                }
+                1 -> {
+                    Toast.makeText(this@HomeActivity, "도로명 주소로 설정", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        alertDialogBuilderBtn.setNegativeButton("취소") { _, _ -> }
+
+        val alertDialogBox = alertDialogBuilderBtn.create()
+        alertDialogBox.show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,7 +229,20 @@ class HomeActivity : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        binding.textMyResident.text = "인천광역시 미추홀구 아암대로 15 인하한양아이클래스 xxxx호"
+        binding.textMyResident.text = "등록된 정보가 없습니다."
+        binding.btnChangeMyResident.setOnClickListener {
+            val alertDialogBuilderBtn = AlertDialog.Builder(this@HomeActivity)
+            alertDialogBuilderBtn.setTitle("내 거주지 변경")
+            alertDialogBuilderBtn.setItems(optionsForChangeMyResidence) { _, which ->
+                when (which) {
+                    0 -> getMyLocation()
+                }
+            }
+            alertDialogBuilderBtn.setNegativeButton("취소") { _, _ -> }
+
+            val alertDialogBox = alertDialogBuilderBtn.create()
+            alertDialogBox.show()
+        }
 
         binding.btnAddDevice.setOnClickListener {
             val intent = Intent(this, AddDeviceActivity::class.java)
