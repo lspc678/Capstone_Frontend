9813204b4e1685a9c41bd7e99372fef9fbccc978
lspc678 <dltmdqls1945@naver.com>
2023-11-02 21:15:42 +0900

    param getActivity is removed and changed to use DeviceTypeList(enum class)

--- app/src/main/java/com/toyproject/ecosave/RecyclerViewRegisteredDeviceListAdapter.kt
+++ app/src/main/java/com/toyproject/ecosave/RecyclerViewRegisteredDeviceListAdapter.kt
@@ -1,24 +1,28 @@
 package com.toyproject.ecosave
 
 import android.annotation.SuppressLint
+import android.content.Intent
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.toyproject.ecosave.models.DeviceTypeList
 
 import com.toyproject.ecosave.models.RelativeElectricPowerConsumeGradeData
 
 class RecyclerViewRegisteredDeviceListAdapter constructor(
-    private val getActivity: HomeActivity,
-    private val list: List<RelativeElectricPowerConsumeGradeData>)
-    : RecyclerView.Adapter<RecyclerViewRegisteredDeviceListAdapter.ViewHolder>()  {
+    private val list: List<RelativeElectricPowerConsumeGradeData>
+) : RecyclerView.Adapter<RecyclerViewRegisteredDeviceListAdapter.ViewHolder>()  {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageDevice: ImageView = itemView.findViewById(R.id.imageDevice)
         val textRelativeElectricPowerConsumeGrade: TextView = itemView.findViewById(R.id.textRelativeElectricPowerConsumeGrade)
         val textPowerOfConsume: TextView = itemView.findViewById(R.id.textPowerOfConsume)
+        val registeredDeviceListItem: CardView = itemView.findViewById(R.id.registeredDeviceListItem)
     }
 
     override fun onCreateViewHolder(
@@ -35,10 +39,10 @@ class RecyclerViewRegisteredDeviceListAdapter constructor(
         position: Int
     ) {
         when (list[position].deviceType) {
-            0 -> holder.imageDevice.setImageResource(R.drawable.img_refrigerator)
-            1 -> holder.imageDevice.setImageResource(R.drawable.img_air_conditioner)
-            2 -> holder.imageDevice.setImageResource(R.drawable.ic_tv)
-            5 -> holder.imageDevice.setImageResource(R.drawable.img_boiler)
+            DeviceTypeList.REFRIGERATOR -> holder.imageDevice.setImageResource(R.drawable.img_refrigerator)
+            DeviceTypeList.AIR_CONDITIONER -> holder.imageDevice.setImageResource(R.drawable.img_air_conditioner)
+            DeviceTypeList.TV -> holder.imageDevice.setImageResource(R.drawable.ic_tv)
+            DeviceTypeList.BOILER -> holder.imageDevice.setImageResource(R.drawable.img_boiler)
             else -> holder.imageDevice.setImageResource(R.drawable.ic_image)
         }
 
@@ -51,6 +55,20 @@ class RecyclerViewRegisteredDeviceListAdapter constructor(
             0 -> holder.textPowerOfConsume.text = list[position].powerOfConsume.toString() + " kWh/월"
             1 -> holder.textPowerOfConsume.text = list[position].powerOfConsume.toString() + "W"
             else -> holder.textPowerOfConsume.text = "Error"
+        }
+
+        holder.registeredDeviceListItem.setOnClickListener {
+            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+
+            when (list[position].deviceType) {
+                DeviceTypeList.REFRIGERATOR -> intent.putExtra("deviceType", "냉장고")
+                DeviceTypeList.AIR_CONDITIONER -> intent.putExtra("deviceType", "에어컨")
+                DeviceTypeList.TV -> intent.putExtra("deviceType", "TV")
+                DeviceTypeList.BOILER -> intent.putExtra("deviceType", "보일러")
+                else -> {}
+            }
+
+            ContextCompat.startActivity(holder.itemView.context, intent, null)
         }
     }
 
