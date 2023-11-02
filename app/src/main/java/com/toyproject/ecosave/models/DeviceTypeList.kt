4e4211dddd7711cee59ca912ff4d9e3f8ba39078
lspc678 <dltmdqls1945@naver.com>
2023-11-02 21:11:11 +0900

    manage device list by enum class

---
+++ app/src/main/java/com/toyproject/ecosave/models/DeviceTypeList.kt
@@ -0,0 +1,10 @@
+package com.toyproject.ecosave.models
+
+enum class DeviceTypeList {
+    REFRIGERATOR,    // 냉장고
+    AIR_CONDITIONER, // 에어컨
+    TV,
+    WASHING_MACHINE, // 세탁기
+    MICROWAVE_OVEN,  // 전자레인지
+    BOILER           // 보일러
+}
