a38c8140ef2fe6d2a04ef4799c779c36d75c265a
lspc678 <dltmdqls1945@naver.com>
2023-11-08 23:23:30 +0900

    added DeviceTypeList.MICROWAVE_OVEN

--- app/src/main/java/com/toyproject/ecosave/utilities/getPowerOfConsumeUnit.kt
+++ app/src/main/java/com/toyproject/ecosave/utilities/getPowerOfConsumeUnit.kt
@@ -8,16 +8,20 @@ fun getPowerOfConsumeUnit(deviceType: DeviceTypeList?) : Map<String, String> {
             "description" to "월간소비전력량",
             "symbol" to "kWh/월"
         )
-        DeviceTypeList.WASHING_MACHINE -> mapOf(
-            "description" to "1kg당 월간소비전력량",
-            "symbol" to "Wh/kg"
-        )
         DeviceTypeList.AIR_CONDITIONER -> mapOf(
             "description" to "월간소비전력량",
             "symbol" to "kWh/월"
         )
         DeviceTypeList.TV -> mapOf(
             "description" to "소비전력",
+            "symbol" to "W"
+        )
+        DeviceTypeList.WASHING_MACHINE -> mapOf(
+            "description" to "1kg당 월간소비전력량",
+            "symbol" to "Wh/kg"
+        )
+        DeviceTypeList.MICROWAVE_OVEN -> mapOf(
+            "description" to "정격소비전력",
             "symbol" to "W"
         )
         DeviceTypeList.BOILER -> mapOf(
