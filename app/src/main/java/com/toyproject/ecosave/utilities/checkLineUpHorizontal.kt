8f4c1d721a871cee033396d1dfb9428e5d4ded62
lspc678 <dltmdqls1945@naver.com>
2023-11-11 23:49:48 +0900

    delete requirement for vertical position

--- app/src/main/java/com/toyproject/ecosave/utilities/checkLineUpHorizontal.kt
+++ app/src/main/java/com/toyproject/ecosave/utilities/checkLineUpHorizontal.kt
@@ -34,12 +34,10 @@ fun checkLineUpHorizontal(rectLeft: Rect?, rectRight: Rect?) : Boolean {
 
     Log.d("라이브프리뷰", "${rectRight.top}, $rectLeftCenterY, ${rectRight.bottom}")
     Log.d("라이브프리뷰", "${rectLeft.top}, $rectRightCenterY, ${rectLeft.bottom}")
-    Log.d("라이브프리뷰", "${rectLeft.right}, ${rectRight.left}")
     Log.d("라이브프리뷰", "===================================")
 
     return ((rectRight.top <= rectLeftCenterY)
             && (rectLeftCenterY <= rectRight.bottom)
             && (rectLeft.top <= rectRightCenterY)
-            && (rectRightCenterY <= rectLeft.bottom)
-            && (rectLeft.right <= rectRight.left))
+            && (rectRightCenterY <= rectLeft.bottom))
 }
