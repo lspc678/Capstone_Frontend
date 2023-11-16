a4e0d5af2bf5768b0b7e91f85bce153040c7bb6f
lspc678 <dltmdqls1945@naver.com>
2023-11-16 22:46:53 +0900

    fix: added microwave oven at getTranslatedDeviceType() (#17)

--- app/src/main/java/com/toyproject/ecosave/utilities/getTranslatedDeviceType.kt
+++ app/src/main/java/com/toyproject/ecosave/utilities/getTranslatedDeviceType.kt
@@ -5,9 +5,10 @@ import com.toyproject.ecosave.models.DeviceTypeList
 fun getTranslatedDeviceType(deviceType: DeviceTypeList?) : String {
     return when (deviceType) {
         DeviceTypeList.REFRIGERATOR -> "냉장고"
-        DeviceTypeList.WASHING_MACHINE -> "세탁기"
         DeviceTypeList.AIR_CONDITIONER -> "에어컨"
         DeviceTypeList.TV -> "TV"
+        DeviceTypeList.WASHING_MACHINE -> "세탁기"
+        DeviceTypeList.MICROWAVE_OVEN -> "전자레인지"
         DeviceTypeList.BOILER -> "보일러"
         else -> ""
     }
