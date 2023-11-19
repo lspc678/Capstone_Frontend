c5651ff6071b198510e4b0a784a6e989dd407a49
lspc678 <dltmdqls1945@naver.com>
2023-11-19 16:15:06 +0900

    chore: added @SerializedName at each variable

--- app/src/main/java/com/toyproject/ecosave/api/responsemodels/GeocodingResponse.kt
+++ app/src/main/java/com/toyproject/ecosave/api/responsemodels/GeocodingResponse.kt
@@ -1,14 +1,23 @@
 package com.toyproject.ecosave.api.responsemodels
 
+import com.google.gson.annotations.SerializedName
+
 data class SearchAddressResponse(
+    @SerializedName("status")
     val status: String,
+    @SerializedName("errorMessage")
     val errorMessage: String,
+    @SerializedName("addresses")
     val addresses: List<AddressResponse>
 )
 
 data class AddressResponse(
+    @SerializedName("roadAddress")
     val roadAddress: String,
+    @SerializedName("jibunAddress")
     val jibunAddress: String,
+    @SerializedName("x")
     val x: String,
+    @SerializedName("y")
     val y: String
 )
--- app/src/main/java/com/toyproject/ecosave/api/responsemodels/ReverseGeocodingResponse.kt
+++ app/src/main/java/com/toyproject/ecosave/api/responsemodels/ReverseGeocodingResponse.kt
@@ -1,28 +1,42 @@
 package com.toyproject.ecosave.api.responsemodels
 
+import com.google.gson.annotations.SerializedName
+
 data class ReverseGeocodingResponse(
+    @SerializedName("results")
     val results: List<AreaResponse>
 )
 
 data class AreaResponse(
+    @SerializedName("name")
     val name: String,
+    @SerializedName("region")
     val region: RegionObject,
+    @SerializedName("land")
     val land: LandObject
 )
 
 data class RegionObject(
+    @SerializedName("area1")
     val area1: AreaObject,
+    @SerializedName("area2")
     val area2: AreaObject,
+    @SerializedName("area3")
     val area3: AreaObject
 )
 
 data class AreaObject(
+    @SerializedName("name")
     val name: String
 )
 
 data class LandObject(
+    @SerializedName("type")
     val type: String,
+    @SerializedName("number1")
     val number1: String,
+    @SerializedName("number2")
     val number2: String,
+    @SerializedName("name")
     val name: String
 )
