ee14008ed6a9e291c57946ca9b555ccf1e2ea4d0
lspc678 <dltmdqls1945@naver.com>
2023-11-19 16:11:07 +0900

    add: added APIClientForServerByPassSSLCertificate, APIClientForNaverMap Class (#24)

--- app/src/main/java/com/toyproject/ecosave/api/APIClient.kt
+++ app/src/main/java/com/toyproject/ecosave/api/APIClient.kt
@@ -36,3 +36,65 @@ class APIClientForServer {
         }
     }
 }
+
+// https 통신을 할 때 SSL 인증서(certificate) 검사를 우회함
+class APIClientForServerByPassSSLCertificate {
+    companion object {
+        // 서버 주소
+        private const val BASE_URL = "http://13.125.246.213:8000/"
+
+        fun getClient() : Retrofit {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            // getUnsafeOkHttpClient()를 이용하여 SSL 인증서 검사를 우회함
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(getUnsafeOkHttpClient().build())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
+
+class APIClientForNaverMap {
+    companion object {
+        // 서버 주소
+        private const val BASE_URL = "https://naveropenapi.apigw.ntruss.com/"
+
+        fun getClient() : Retrofit {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val headerInterceptor = Interceptor {
+                val request = it.request()
+                    .newBuilder()
+                    .build()
+                return@Interceptor it.proceed(request)
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(headerInterceptor)
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
