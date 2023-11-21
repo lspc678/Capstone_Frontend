649fa0cb9d73da9658c8bbd899919bb0080911f2
lspc678 <dltmdqls1945@naver.com>
2023-11-21 16:52:35 +0900

    chore: changed model name and added comments

--- app/src/main/java/com/toyproject/ecosave/models/RegisteredDeviceData.kt
+++ app/src/main/java/com/toyproject/ecosave/models/RegisteredDeviceData.kt
@@ -1,11 +1,26 @@
 package com.toyproject.ecosave.models
 
-data class RelativeGradeData(
+data class RegisteredDeviceData(
+    // 기기 종류
     val deviceType: DeviceTypeList,
+    
+    // 전력 소비 상대 등급
     val relativeElectricPowerConsumeGrade: Int,
+
+    // 전력 소비 누적 비율(%)
     val relativeElectricPowerConsumePercentage: Int,
+    
+    // 전력 소비량
     val powerOfConsume: Float,
+
+    // CO2 배출량이 적혀있지 않은 제품의 경우 CO2 배출량 상대 등급과 누적 비율(%)은 동일하다고 가정
+
+    // CO2 배출량 상대 등급
     val relativeCO2EmissionGrade: Int?,
+
+    // CO2 배출량 누적 비율(%)
     val relativeCO2EmissionPercentage: Int?,
+
+    // CO2 배출량
     val amountOfCO2Emission: Float?,
 )
