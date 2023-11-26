e04fdc46ad82fc162f1fd9bf24b3680e125f4958
lspc678 <dltmdqls1945@naver.com>
2023-11-26 23:57:10 +0900

    chore: email is changed to mail, added code attribute

--- app/src/main/java/com/toyproject/ecosave/api/requestmodels/SignUpRequest.kt
+++ app/src/main/java/com/toyproject/ecosave/api/requestmodels/SignUpRequest.kt
@@ -3,14 +3,16 @@ package com.toyproject.ecosave.api.requestmodels
 import com.google.gson.annotations.SerializedName
 
 data class SignUpRequest(
-    @SerializedName("email")
-    val email: String,
+    @SerializedName("mail")
+    val mail: String,
     @SerializedName("pw1")
     val pw1: String,
     @SerializedName("pw2")
     val pw2: String,
     @SerializedName("nickname")
     val nickname: String,
+    @SerializedName("code")
+    val code: String,
     @SerializedName("longitude")
     val longitude: Double,
     @SerializedName("latitude")
