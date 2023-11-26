07a15dceedb0390917274b0ed02465cdf4242279
lspc678 <dltmdqls1945@naver.com>
2023-11-27 00:05:50 +0900

    chore: added id, model field, some fields type is changed (#30)

--- app/src/main/java/com/toyproject/ecosave/models/RegisteredDeviceData.kt
+++ app/src/main/java/com/toyproject/ecosave/models/RegisteredDeviceData.kt
@@ -1,28 +1,34 @@
 package com.toyproject.ecosave.models
 
 data class RegisteredDeviceData(
+    // pkey
+    val id: Int,
+
     // 기기 종류
     val deviceType: DeviceTypeList,
+
+    // 모델명
+    var model: String?,
     
     // 전력 소비 상대 등급
-    val relativeElectricPowerConsumeGrade: Int,
+    var relativeElectricPowerConsumeGrade: Int?,
 
     // 전력 소비 누적 비율(%)
-    val relativeElectricPowerConsumePercentage: Int,
+    var relativeElectricPowerConsumePercentage: Int?,
     
     // 전력 소비량
-    val powerOfConsume: Float,
+    var powerOfConsume: Double?,
 
     // CO2 배출량이 적혀있지 않은 제품의 경우 CO2 배출량 상대 등급과 누적 비율(%)은 동일하다고 가정
 
     // CO2 배출량 상대 등급
-    val relativeCO2EmissionGrade: Int?,
+    var relativeCO2EmissionGrade: Int?,
 
     // CO2 배출량 누적 비율(%)
-    val relativeCO2EmissionPercentage: Int?,
+    var relativeCO2EmissionPercentage: Int?,
 
     // CO2 배출량
-    val amountOfCO2Emission: Float?,
+    var amountOfCO2Emission: Double?,
 
     // 하루 평균 사용 시간
     var averageUsageTimePerDay: Float?
