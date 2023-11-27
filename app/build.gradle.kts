9fdf48f2a4a7caf2c85c0ee89901ae64cea12db1
JoSeonggwon <q05008@naver.com>
2023-11-27 21:31:57 +0900

    add RecommendProductData Crawling function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Crawling
+    implementation("org.jsoup:jsoup:1.13.1")
+
 }
