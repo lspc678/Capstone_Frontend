e396de02c0ea27aab294c9aacb5801f98e0e133e
lspc678 <dltmdqls1945@naver.com>
2023-11-28 11:52:48 +0900

    feat: Add Glide package to display device photos, set to display power consumption after changing device in SimulationActivity when clicking on recommended product (#32)

--- app/src/main/java/com/toyproject/ecosave/RecyclerViewProductRecommendationListAdapter.kt
+++ app/src/main/java/com/toyproject/ecosave/RecyclerViewProductRecommendationListAdapter.kt
@@ -2,14 +2,18 @@ package com.toyproject.ecosave
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+
+import com.bumptech.glide.Glide
 import com.toyproject.ecosave.models.DeviceTypeList
 import com.toyproject.ecosave.models.RecommendProductData
+import com.toyproject.ecosave.utilities.fromDpToPx
 import com.toyproject.ecosave.utilities.getPowerOfConsumeUnit
 
 import java.math.BigDecimal
@@ -18,13 +22,30 @@ import java.math.RoundingMode
 class RecyclerViewProductRecommendationListAdapter constructor(
     private val context: Context,
     private val productRecommendationList: List<RecommendProductData>,
-    private val deviceType: DeviceTypeList
+    private val deviceType: DeviceTypeList,
+    private val resources: Resources
 ) : RecyclerView.Adapter<RecyclerViewProductRecommendationListAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    interface OnItemClickListener {
+        fun onItemClick(v:View, data: RecommendProductData, pos: Int)
+    }
+
+    private var listener : OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener : OnItemClickListener) {
+        this.listener = listener
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageDevice: ImageView = itemView.findViewById(R.id.imageDevice)
         val textProductName: TextView = itemView.findViewById(R.id.textProductName)
         val textPowerOfConsumeType: TextView = itemView.findViewById(R.id.textPowerOfConsumeType)
         val textPowerOfConsume: TextView = itemView.findViewById(R.id.textPowerOfConsume)
+
+        fun bind(item: RecommendProductData, position: Int) {
+            itemView.setOnClickListener {
+                listener?.onItemClick(itemView, item, position)
+            }
+        }
     }
 
     override fun onCreateViewHolder(
@@ -40,8 +61,21 @@ class RecyclerViewProductRecommendationListAdapter constructor(
         holder: ViewHolder,
         position: Int
     ) {
+        // 제품 이미지 표시
+        val url = "https:${productRecommendationList[position].imageUrl}"
+        val width = fromDpToPx(resources, 110.0F)
+        val height = fromDpToPx(resources, 155.0F)
+        Glide
+            .with(context)
+            .load(url)
+            .override(width, height)
+            .error(R.drawable.ic_image_not_supported)
+            .into(holder.imageDevice)
+
         // 제품 이름 표시
         holder.textProductName.text = productRecommendationList[position].productName
+
+        holder.bind(productRecommendationList[position], position)
 
         // 소비전력량 표시
         holder.textPowerOfConsumeType.text = getPowerOfConsumeUnit(deviceType)["description"]
@@ -57,4 +91,9 @@ class RecyclerViewProductRecommendationListAdapter constructor(
     override fun getItemCount(): Int {
         return productRecommendationList.size
     }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
 }
