d2f2cd616b294a431f505ce3bf783c775b773aa8
lspc678 <dltmdqls1945@naver.com>
2023-11-28 11:53:41 +0900

    add: Added a function to create a dialog with only an OK button

--- app/src/main/java/com/toyproject/ecosave/widget/createDialog.kt
+++ app/src/main/java/com/toyproject/ecosave/widget/createDialog.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AlertDialog
 
+// 확인, 취소 버튼이 있는 dialog
 fun createDialog(
     context: Context,
     title: String,
@@ -17,6 +18,23 @@ fun createDialog(
 
     alertDialogBuilderBtn.setPositiveButton("확인", positiveButtonOnClickListener)
     alertDialogBuilderBtn.setNegativeButton("취소", negativeButtonOnClickListener)
+
+    val alertDialogBox = alertDialogBuilderBtn.create()
+    alertDialogBox.show()
+}
+
+// 확인 버튼만 존재하는 dialog
+fun createDialog(
+    context: Context,
+    title: String,
+    message: String,
+    positiveButtonOnClickListener: DialogInterface.OnClickListener,
+) {
+    val alertDialogBuilderBtn = AlertDialog.Builder(context)
+    alertDialogBuilderBtn.setTitle(title)
+    alertDialogBuilderBtn.setMessage(message)
+
+    alertDialogBuilderBtn.setPositiveButton("확인", positiveButtonOnClickListener)
 
     val alertDialogBox = alertDialogBuilderBtn.create()
     alertDialogBox.show()
