4e3b606a2a07a7625c8ccc35f444f52382dffa62
lspc678 <dltmdqls1945@naver.com>
2023-11-28 11:41:34 +0900

    feat, remove: Implementation add device, delete unnecessary comments (#30)

--- app/src/main/java/com/toyproject/ecosave/AddDeviceActivity.kt
+++ app/src/main/java/com/toyproject/ecosave/AddDeviceActivity.kt
@@ -12,7 +12,8 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.toyproject.ecosave.api.APIClientForServer
+
+import com.toyproject.ecosave.api.APIClientForServerByPassSSLCertificate
 import com.toyproject.ecosave.api.APIInterface
 import com.toyproject.ecosave.api.requestmodels.AppliancePostRequest
 import com.toyproject.ecosave.api.responsemodels.DefaultResponse
@@ -25,6 +26,7 @@ import com.toyproject.ecosave.widget.ProgressDialog
 import com.toyproject.ecosave.widget.createDialog
 import com.toyproject.ecosave.widget.defaultNegativeDialogInterfaceOnClickListener
 import com.toyproject.ecosave.widget.simpleDialog
+
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -32,17 +34,11 @@ import java.io.File
 
 class AddDeviceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddDeviceBinding
-    private var photoFile: File? = null
-    private var mCurrentPhotoPath: String? = null
-    private lateinit var finalBitmap: Bitmap
-    private lateinit var photoURI: Uri
 
     private var energyConsumption = 0.0F
     private var amountOfCO2 = 0.0F
 
     companion object {
-        private const val REQUEST_CAMERA_PERMISSION = 1000
-        private const val CAPTURE_IMAGE_REQUEST = 1
         private const val GET_ENERGY_CONSUMPTION_AND_CO2 = 50
     }
 
@@ -60,7 +56,7 @@ class AddDeviceActivity : AppCompatActivity() {
         progressDialog.show()
 
         val type = getTranslatedDeviceType(deviceType)
-        val apiInterface = APIClientForServer
+        val apiInterface = APIClientForServerByPassSSLCertificate
             .getClient()
             .create(APIInterface::class.java)
 
@@ -72,7 +68,7 @@ class AddDeviceActivity : AppCompatActivity() {
                     AppliancePostRequest(
                         energyConsumption.toDouble(),
                         amountOfCO2.toDouble(),
-                        type
+                        "none"
                     )
                 )
             }
@@ -81,7 +77,7 @@ class AddDeviceActivity : AppCompatActivity() {
                     AppliancePostRequest(
                         energyConsumption.toDouble(),
                         amountOfCO2.toDouble(),
-                        type
+                        "none"
                     )
                 )
             }
@@ -90,7 +86,7 @@ class AddDeviceActivity : AppCompatActivity() {
                     AppliancePostRequest(
                         energyConsumption.toDouble(),
                         amountOfCO2.toDouble(),
-                        type
+                        "none"
                     )
                 )
             }
@@ -99,7 +95,7 @@ class AddDeviceActivity : AppCompatActivity() {
                     AppliancePostRequest(
                         energyConsumption.toDouble(),
                         amountOfCO2.toDouble(),
-                        type
+                        "none"
                     )
                 )
             }
@@ -108,7 +104,7 @@ class AddDeviceActivity : AppCompatActivity() {
                     AppliancePostRequest(
                         energyConsumption.toDouble(),
                         amountOfCO2.toDouble(),
-                        type
+                        "none"
                     )
                 )
             }
@@ -117,7 +113,7 @@ class AddDeviceActivity : AppCompatActivity() {
                     AppliancePostRequest(
                         energyConsumption.toDouble(),
                         amountOfCO2.toDouble(),
-                        type
+                        "none"
                     )
                 )
             }
@@ -140,7 +136,7 @@ class AddDeviceActivity : AppCompatActivity() {
                         val result = response.body()
 
                         if (result != null) {
-                            if ((result.success) && (result.message == "전송성공")) {
+                            if (result.success) {
                                 Log.d("기기등록 ($type)", "결과: 성공")
                                 Log.d("기기등록 ($type)", result.toString())
 
@@ -193,90 +189,6 @@ class AddDeviceActivity : AppCompatActivity() {
         )
     }
 
-//    private fun getPermissionForCamera() {
-//        val cameraPermission = ContextCompat.checkSelfPermission(
-//            this,
-//            android.Manifest.permission.CAMERA
-//        )
-//
-//        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
-//            // 카메라 권한이 없는 경우 권한 요청
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.CAMERA)) {
-//                ActivityCompat.requestPermissions(
-//                    this,
-//                    arrayOf(android.Manifest.permission.CAMERA),
-//                    REQUEST_CAMERA_PERMISSION
-//                )
-//            } else {
-//                // 카메라 권한 거부 및 '다시 묻지 않음'인 경우
-//                val alertDialogBuilderBtn = AlertDialog.Builder(this)
-//                alertDialogBuilderBtn.setTitle("카메라 권한 요청")
-//                alertDialogBuilderBtn.setMessage("카메라 권한이 거부되었습니다. 설정(앱 정보)에서 카메라 권한을 허용해 주세요.")
-//                alertDialogBuilderBtn.setPositiveButton("확인") { _, _ ->
-//                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-//                    val uri = Uri.fromParts("package", packageName, null)
-//                    intent.data = uri
-//                    startActivity(intent)
-//                }
-//                alertDialogBuilderBtn.setNegativeButton("취소") { _, _ -> }
-//
-//                val alertDialogBox = alertDialogBuilderBtn.create()
-//                alertDialogBox.show()
-//            }
-//        } else {
-//            // 카메라 권한이 있음
-//            captureImage()
-//        }
-//    }
-
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == REQUEST_CAMERA_PERMISSION) {
-//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                captureImage()
-//            }
-//        }
-//    }
-
-//    private fun captureImage() {
-//        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//        if (takePictureIntent.resolveActivity(packageManager) != null) {
-//            // Create the File where the photo should go
-//            try {
-//                photoFile = createImageFile()
-//                // Continue only if the File was successfully created
-//                if (photoFile != null) {
-//                    photoURI = FileProvider.getUriForFile(
-//                        this,
-//                        "com.toyproject.ecosave.fileprovider",
-//                        photoFile!!
-//                    )
-//                    takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-//                    startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST)
-//                }
-//            } catch (ex: Exception) {
-//                // Error occurred while creating the File
-//                Log.d("사진 촬영", ex.message.toString())
-//                Toast.makeText(
-//                    baseContext,
-//                    "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.",
-//                    Toast.LENGTH_LONG
-//                ).show()
-//            }
-//        } else {
-//            Toast.makeText(
-//                baseContext,
-//                "Null",
-//                Toast.LENGTH_SHORT
-//            ).show()
-//        }
-//    }
-
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -299,114 +211,7 @@ class AddDeviceActivity : AppCompatActivity() {
                 Log.d("기기추가", "null")
             }
         }
-
-//        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-//            var exif: ExifInterface? = null
-//
-//            try {
-//                exif = ExifInterface(photoFile!!.absolutePath)
-//            } catch (ex: IOException) {
-//                ex.printStackTrace()
-//            }
-//
-//            val orientation = exif?.getAttributeInt(
-//                ExifInterface.TAG_ORIENTATION,
-//                ExifInterface.ORIENTATION_UNDEFINED
-//            )
-//
-//            val bitmap = BitmapFactory.decodeFile(photoFile!!.absolutePath)
-//            val cameraRotated = rotateBitmap(bitmap, orientation)
-//
-//            if (cameraRotated != null) {
-//                finalBitmap = cameraRotated
-//                binding.deviceImage.setImageURI(photoURI)
-//                recognizeText()
-//            }
-//        }
     }
-
-//    @SuppressLint("SimpleDateFormat")
-//    @Throws(IOException::class)
-//    private fun createImageFile() : File? {
-//        // Create an image file name
-//        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-//        val imageFileName = "JPEG_" + timeStamp + "_"
-//        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-//        val image = File.createTempFile(
-//            imageFileName, // prefix
-//            ".jpg", // suffix
-//            storageDir // directory
-//        )
-//
-//        // Save a file: path for use with ACTION_VIEW intents
-//        mCurrentPhotoPath = image.absolutePath
-//        return image
-//    }
-
-//    private fun rotateBitmap(bitmap: Bitmap, orientation: Int?): Bitmap? {
-//        val matrix = Matrix()
-//        when (orientation) {
-//            ExifInterface.ORIENTATION_NORMAL -> return bitmap
-//            ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> matrix.setScale(-1F, 1F)
-//            ExifInterface.ORIENTATION_ROTATE_180 -> matrix.setRotate(180F)
-//            ExifInterface.ORIENTATION_FLIP_VERTICAL -> {
-//                matrix.setRotate(180F)
-//                matrix.postScale(-1F, 1F)
-//            }
-//            ExifInterface.ORIENTATION_TRANSPOSE -> {
-//                matrix.setRotate(90F)
-//                matrix.postScale(-1F, 1F)
-//            }
-//            ExifInterface.ORIENTATION_ROTATE_90 -> matrix.setRotate(90F)
-//            ExifInterface.ORIENTATION_TRANSVERSE -> {
-//                matrix.setRotate(-90F)
-//                matrix.postScale(-1F, 1F)
-//            }
-//            ExifInterface.ORIENTATION_ROTATE_270 -> matrix.setRotate(-90F)
-//            else -> return bitmap
-//        }
-//
-//        return try {
-//            val bmRotated: Bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-//            bitmap.recycle()
-//            bmRotated
-//        } catch (ex: OutOfMemoryError) {
-//            ex.printStackTrace()
-//            null
-//        }
-//    }
-//
-//    private fun recognizeText() {
-//        val recognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
-//
-//        // val image = InputImage.fromBitmap(finalBitmap, 0)
-//        try {
-//            val image = InputImage.fromFilePath(this, photoURI)
-//            Log.d("인식", photoURI.toString())
-//            // [START run_detector]
-//            val result = recognizer.process(image)
-//                .addOnSuccessListener {
-//                    processTextBlock(it)
-//                }
-//                .addOnFailureListener { e ->
-//                    e.printStackTrace()
-//                }
-//            // [END run_detector]
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//    }
-//
-//    private fun processTextBlock(result: Text) {
-//        for (block in result.textBlocks) {
-//            for (line in block.lines) {
-//                Log.d("인식(line)", line.text)
-//                for (element in line.elements) {
-//
-//                }
-//            }
-//        }
-//    }
 
     @androidx.camera.core.ExperimentalGetImage
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -527,39 +332,6 @@ class AddDeviceActivity : AppCompatActivity() {
                     "사진 촬영이 진행되지 않았습니다."
                 )
             }
-
-//            if (cameraRotated == null) {
-//                simpleDialog(
-//                    this,
-//                    "기기 추가",
-//                    "에너지 소비 등급 라벨을 촬영해 주세요."
-//                )
-//            } else {
-//                if (energyConsumption == 0.0) {
-//                    simpleDialog(
-//                        this,
-//                        "기기 추가",
-//                        "해당 기기의 에너지 소비전력을 인식하지 못했습니다. 다시 촬영해 주세요."
-//                    )
-//                } else {
-//                    if ((binding.spinner.selectedItemPosition == 0) ||
-//                        (binding.spinner.selectedItemPosition == 1) ||
-//                        (binding.spinner.selectedItemPosition == 2) ||
-//                        (binding.spinner.selectedItemPosition == 3)) {
-//                        if (amountOfCO2 == 0.0) {
-//                            simpleDialog(
-//                                this,
-//                                "기기 추가",
-//                                "해당 기기의 CO2 발생량을 인식하지 못했습니다. 다시 촬영해 주세요."
-//                            )
-//                        } else {
-//                            //
-//                        }
-//                    } else {
-//                        //
-//                    }
-//                }
-//            }
         }
     }
 
